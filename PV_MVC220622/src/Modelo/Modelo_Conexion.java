cbee72bf80caf356b45b034a3a309915faf38e3e
PC ESCRITORIO <fernando95f4@gmail.com>
2022-06-27 20:22:28 -0500

    Archivo faltante.

---
+++ PV_MVC220622/src/Modelo/Modelo_Conexion.java
@@ -0,0 +1,28 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Modelo_Conexion 
+{
+    public Connection conexionBD()
+    {
+        String baseDatos = "jdbc:postgresql://localhost:5432/veterinaria_pv_270622";
+        String user = "postgres";
+        String password = "FerDatos95";
+        try 
+        {
+            System.out.println("Conexion exitosa.");
+//            Conexion a la base de datos.
+            Connection conexion = DriverManager.getConnection(baseDatos, user, password);
+            conexion.close();
+            return conexion;
+        } catch (SQLException ex) 
+        {
+            JOptionPane.showMessageDialog(null, "Error al conectar a la base de datos: " + ex);
+            return null;
+        }
+    }
+}
