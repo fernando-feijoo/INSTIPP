4c00d4a0d4f6a40fa0f3cda239926ebc8481887c
PC ESCRITORIO <fernando95f4@gmail.com>
2022-07-03 15:32:16 -0500

    Local PC Escritorio

--- PV_MVC220622/src/Modelo/Main.java
+++ PV_MVC220622/src/Modelo/Main.java
@@ -2,7 +2,7 @@ package Modelo;
 
 public class Main 
 {
-    public static void main(String[] args) 
+    public static void main(String args[]) 
     {
         Modelo_Conexion bd = new Modelo_Conexion();
         bd.conexionBD();
