f917f16ea0be3d84d9de90247cf51bb6cd72bdfc
PC PORTATIL <fernando95f4@gmail.com>
2022-08-07 11:24:24 -0500

    Actualizacion, nuevo deber.

---
+++ Laboratorio15_060822/src/laboratorio15_060822/Main.java
@@ -0,0 +1,9 @@
+package laboratorio15_060822;
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Menu menu = new Menu();
+        menu.menu();
+    }
+}
---
+++ Laboratorio15_060822/src/laboratorio15_060822/ValorException.java
@@ -0,0 +1,10 @@
+package laboratorio15_060822;
+public class ValorException extends Exception
+{
+
+    public ValorException() 
+    {
+        super("CATIDAD NO VÁLIDA");
+    }
+    
+}
