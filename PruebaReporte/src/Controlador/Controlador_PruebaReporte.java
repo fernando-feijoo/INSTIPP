e7aa007c63490dcc04b4aa16388b938bb1c5686d
PC PORTATIL <fernando95f4@gmail.com>
2022-08-31 19:07:36 -0500

    Actualizacion previa local Portatil

--- PruebaReporte/src/Controlador/Controlador_PruebaReporte.java
+++ PruebaReporte/src/Controlador/Controlador_PruebaReporte.java
@@ -1,12 +1,19 @@
 package Controlador;
 
+import Modelo.Modelo_Conexion;
 import Vista.Vista_PruebaReporte;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 public class Controlador_PruebaReporte implements ActionListener {
-    Vista_PruebaReporte vistaPrueba;
 
+    Vista_PruebaReporte vistaPrueba;
+    Modelo_Conexion modeloConexion = new Modelo_Conexion();
     public Controlador_PruebaReporte(Vista_PruebaReporte vistaPrueba) {
         this.vistaPrueba = vistaPrueba;
         this.vistaPrueba.btn_imprimir.addActionListener(this);
@@ -15,9 +22,19 @@ public class Controlador_PruebaReporte implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == vistaPrueba.btn_imprimir) {
-            JasperReport reporte;
-            JasperPrint jprint;
-            reporte = JRLoader.loadObject(getClass().getResource("/Reportes/Pacientes_Reporte.jasper"));
+            try {
+                JasperReport reporte;
+                JasperPrint jprint = null;
+                reporte = (JasperReport) JRLoader.loadObject(getClass().getResource("/Reportes/Pacientes_Reporte.jasper"));
+                jprint = JasperFillManager.fillReport(reporte, null, modeloConexion.conexion_bd());
+                if (jprint != null) {
+                    JasperViewer view = new JasperViewer(jprint);
+                    view.setVisible(true);
+                }
+            } catch (Exception ex) {
+                System.out.println("Error: " + ex);
+            }
+
         }
     }
 }
