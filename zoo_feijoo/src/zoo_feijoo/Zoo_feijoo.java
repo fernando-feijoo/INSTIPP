ce5ff767ef7a04167d15fcb892a50584e4410c3c
Fernando PC-Portatil <fernando95f4@gmail.com>
2023-06-07 22:27:13 -0500

    Nuevo proyecto con webservice y consumo en java para listar Examen TDS

---
+++ zoo_feijoo/src/zoo_feijoo/Zoo_feijoo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoo_feijoo;
+
+/**
+ *
+ * @author ANONYMOUS
+ */
+public class Zoo_feijoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        interfaz dashboard = new interfaz();
+        dashboard.listarDatos();
+        dashboard.show();
+    }
+    
+}
